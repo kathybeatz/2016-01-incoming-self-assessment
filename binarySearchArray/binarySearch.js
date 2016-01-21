5fbd7713e4fc4ec1d396d44ce2e6a021ec4af102
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-21 13:34:54 -0800

    Psuedocode for binarySearch

--- binarySearchArray/binarySearch.js
+++ binarySearchArray/binarySearch.js
@@ -8,6 +8,11 @@
  * console.log(index); // [3]
  */
 
+//function should take in an array and a target (what you are looking for)
 var binarySearch = function (array, target) {
- // your code here
+	//create a results container to hold each index that the target is found at
+	//iterate through the array
+		//check the target against each item in the array
+			//if found, push the index at where it was found in the results container
+	//return the results container
 };

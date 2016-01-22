3dbb6ed245b316ae33d1cc81d8a030cbfb9ad8d9
Kathleen L <kathleeng.lopez@gmail.com>
2016-01-22 00:11:21 -0800

    Previous code was for a linear search, woops

--- binarySearchArray/binarySearch.js
+++ binarySearchArray/binarySearch.js
@@ -8,18 +8,7 @@
  * console.log(index); // [3]
  */
 
-//function should take in an array and a target (what you are looking for)
+
 var binarySearch = function (array, target) {
-	//create a results container to hold each index that the target is found at
-	var results = [];
-	//iterate through the array
-	for(var i = 0; i < array.length; i++){
-		//check the target against each item in the array
-		if(array[i] === target){
-			//if found, push the index at where it was found in the results container
-			results.push(i);
-		}
-	}
-	//return the results container
-	return results;
+
 };
